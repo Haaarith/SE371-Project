17a7d8ceac4f60da83ea5b2bad2b43ecd6bf8ca3
Harith <harithful2000@gmail.com>
2022-05-29 23:33:23 +0300

    add footer with fonaweseom e icon

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,68 @@
 <hr />
 <!-- Footer -->
 <footer>
-  <div class="row">
-    <div class="col-lg-12">
-      <p>Copyright &copy; SE371 2022</p>
-    </div>
-  </div>
-</footer>
-
-
+  
 </div>
 <!-- /.container -->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
 
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
+
+
+<footer class="navbar navbar-inverse text-center ">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/PSU_RUH?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" role="button"
+        ><i class="fa fa-twitter"></i
+      ></a>
+
+      <!-- youtube -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/c/PSUofficial" role="button"
+        ><i class="fa fa-fw fa-youtube"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/faisal-alagla-54ba8b235/
+" role="button"
+        ><i class="fa fa-fw fa-linkedin"></i
+      ></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Haaarith/SE371-Project" role="button"
+        ><i class="fa fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">SE371 </a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -86,6 +86,7 @@
 
               ?>
 
+
 <?php 
   include "includes/footer.php";
   ob_flush();
