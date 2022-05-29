0464225655f5f1df0ea17b637afcf18a56b29e99
Faisal-Alagla <alagla.faisal@gmail.com>
2022-05-29 22:27:35 +0300

    Admin pannel modifications

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,51 +1,31 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
-  <ul class="nav navbar-nav side-nav">
-    <li>
-      <!-- link to homepage -->
-      <a href="../index.php"><i class="fa fa-fw fa fa-home"></i> Homepage</a>
-    </li>
-
-    <li>
-      <!-- View all posts in the database -->
-      <a href="./posts.php"><i class="fa fa-fw fa fa-bars"></i>Posts</a>
-    </li>
-
-
-
-    <li>
-      <a href="#"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-    </li>
-    <li>
-      <a href="#"><i class="fa fa-fw fa-table"></i> Tables</a>
-    </li>
-    <li>
-      <a href="#"><i class="fa fa-fw fa-edit"></i> Forms</a>
-    </li>
-    <li>
-      <a href="#"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-    </li>
-    <li>
-      <!--  View all the categories in the database -->
-      <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
-    </li>
-
-    <li>
-      <a href="javascript:;" data-toggle="collapse" data-target="#demo2"><i class="fa fa-fw fa-user"></i> Users
-        <i class="fa fa-fw fa-caret-down"></i></a>
-      <ul id="demo2" class="collapse">
+    <ul class="nav navbar-nav side-nav">
         <li>
-          <a href="./users.php?source=view_all_users">View All users</a>
+            <!-- link to homepage -->
+            <a href="../index.php"><i class="fa fa-fw fa fa-home"></i> Homepage</a>
         </li>
+
         <li>
-          <a href="./users.php?source=add_user">Add user</a>
+            <!-- View all posts in the database -->
+            <a href="./posts.php"><i class="fa fa-fw fa fa-bars"></i>Posts</a>
         </li>
-      </ul>
-    </li>
-    <li>
-      <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
-    </li>
-    <li>
-      <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
-    </li>
-  </ul>
+
+        <li>
+            <!--  View all the categories in the database -->
+            <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+        </li>
+
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo2"><i class="fa fa-fw fa-user"></i> Users
+                <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="demo2" class="collapse">
+                <li>
+                    <a href="./users.php?source=view_all_users">View All users</a>
+                </li>
+                <li>
+                    <a href="./users.php?source=add_user">Add user</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </div>
