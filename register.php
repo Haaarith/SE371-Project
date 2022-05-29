17835d82fdf175393a68d355c244e41d17d9ad86
Harith <harithful2000@gmail.com>
2022-05-29 22:16:45 +0300

    Added Navi bar to login and register page

--- register.php
+++ register.php
@@ -1,80 +1,67 @@
 <?php 
+  session_start();
+  ob_start();
   include "includes/header.php";
   include_once "includes/db.php";
-  session_start();
+  include "includes/navigation.php";
+  
 ?>
 
-
-<section class="vh-100" style="background-color: #eee;">
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
-          <div class="card-body p-md-5">
-            <div class="row justify-content-center">
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
-
-
-
-
-
-                <form class="mx-1 mx-md-4" method="POST">
-
-                  <div class="d-flex flex-row align-items-center mb-5">
-                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="text" name="username" placeholder="username" id="form3Example1c" required
+<div class="well">
+    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign Up</p>
+    <form class="" method="POST">
+        <div class="align-items-center ">
+            <!--Div for centring the form  -->
+            <div style="display: flex; align-items:center; flex-direction:column; gap:15px">
+                <div class="form-outline" style="width: 35%;">
+                    <input type="text" name="username" placeholder="username" id="form3Example1c" required
                         class="form-control" />
-                    </div>
-                  </div>
+                </div>
+                <!--  -->
 
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="email" name="email" placeholder="email" id="form3Example1c" required
+                <!--  -->
+                <div class="form-outline " style="width: 35%;">
+                    <input type="email" name="email" placeholder="email" id="form3Example1c" required
                         class="form-control" />
-                    </div>
-                  </div>
+                </div>
+                <!--  -->
 
-
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="password" name="password" placeholder="password" id="form3Example1c" required
+                <!--  -->
+                <div class="form-outline" style="width: 35%;">
+                    <input type="password" name="password" placeholder="password" id="form3Example1c" required
                         class="form-control" />
-                    </div>
-                  </div>
+                </div>
+                <!--  -->
 
-
-
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="password" placeholder="repeat your password" name="re_password" required
+                <!--  -->
+                <div class="form-outline" style="width: 35%;">
+                    <input type="password" placeholder="repeat your password" name="re_password" required
                         id="form3Example4cd" class="form-control" />
-                    </div>
-                  </div>
+                </div>
+                <!--  -->
 
-                  <div class="form-check d-flex justify-content-center mb-5">
+                <!--  -->
+                <div class="form-check " style="width: 35%;">
                     <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
                     <label class="form-check-label" for="form2Example3">
-                      I agree all statements in daslkdhsadasda <a href="#!">Terms of service</a>
+                        I agree to all the Terms and Conditions <a href="#!">Terms of service</a>
                     </label>
-                  </div>
+                </div>
+                <!--  -->
 
-                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                <!--  -->
+                <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                     <button type="submit" name="register" class="btn btn-primary btn-lg">Register</button>
-                  </div>
+                </div>
+                <!--  -->
+            </div>
+        </div>
 
-                </form>
+    </form>
+</div>
 
-                <?php
 
+<?php
                     if(isset($_POST['register'])) {
                       $username = $_POST['username'];
                       $email = $_POST['email'];
@@ -114,28 +101,9 @@
                         }
                       }
                     }
-
               ?>
-
-              </div>
-              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                  class="img-fluid" alt="Sample image">
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-
 
 <?php 
   include "includes/footer.php";
-
-
+  ob_flush();
 ?>
